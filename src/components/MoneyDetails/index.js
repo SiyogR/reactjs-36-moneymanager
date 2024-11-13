6d6bf3aa5dd537631a22abacb148160e0a352c3b
SiyogR <rsiyog108@gmail.com>
2024-11-13 04:14:51 +0000

    first commit

--- src/components/MoneyDetails/index.js
+++ src/components/MoneyDetails/index.js
@@ -1,52 +1,48 @@
 // Write your code here
+
 import './index.css'
 
 const MoneyDetails = props => {
-  const {balanceAmount, incomeAmount, expensiveAmount} = props
+  const {balanceAmount, incomeAmount, expensesAmount} = props
 
   return (
-    <div className="money-container">
+    <div className="money-details-container">
       <div className="balance-container">
         <img
-          src="https://assets.ccbp.in/frontend/react-js/money-manager/balance-image.png "
+          src="https://assets.ccbp.in/frontend/react-js/money-manager/balance-image.png"
           alt="balance"
-          className="detail-img"
+          className="details-img"
         />
         <div>
-          <p className="paragraph"> Your Balance</p>
-          <p className="paragraph2" data-testid="balanceAmount">
-            {' '}
-            RS {balanceAmount}
+          <p className="details-text">Your Balance</p>
+          <p className="details-money" data-testid="balanceAmount">
+            Rs {balanceAmount}
           </p>
         </div>
       </div>
-
       <div className="income-container">
         <img
-          src="https://assets.ccbp.in/frontend/react-js/money-manager/income-image.png "
+          src="https://assets.ccbp.in/frontend/react-js/money-manager/income-image.png"
           alt="income"
-          className="detail-img"
+          className="details-img"
         />
         <div>
-          <p className="paragraph"> Your Income</p>
-          <p className="paragraph2" data-testid="incomeAmount">
-            {' '}
-            RS {incomeAmount}
+          <p className="details-text">Your Income</p>
+          <p className="details-money" data-testid="incomeAmount">
+            Rs {incomeAmount}
           </p>
         </div>
       </div>
-
       <div className="expenses-container">
         <img
           src="https://assets.ccbp.in/frontend/react-js/money-manager/expenses-image.png"
-          alt="balance"
-          className="detail-img"
+          alt="expenses"
+          className="details-img"
         />
         <div>
-          <p className="paragraph"> Your expenses</p>
-          <p className="paragraph2" data-testid="expensiveAmount">
-            {' '}
-            RS {expensiveAmount}
+          <p className="details-text">Your Expenses</p>
+          <p className="details-money" data-testid="expensesAmount">
+            Rs {expensesAmount}
           </p>
         </div>
       </div>
